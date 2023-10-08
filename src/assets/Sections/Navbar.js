ef19f6d9a4322d31765f881256a13071a35388b3
Usman Javed <uj123243@gmail.com>
2023-10-08 16:13:00 +0500

    Start

---
+++ src/assets/Sections/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className='navbar'>
+            <div className="Name">
+                Usman Javed
+            </div>
+            <div className="as_A">
+                As web Developer
+            </div>
+            <div className="menue">
+                <ul>
+                    <li>Menu</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
